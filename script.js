03e288588cedfcabaf4d42e5b0e30b45bebc1975
Hunter Tran <4468525+huntertran@users.noreply.github.com>
2024-04-30 23:12:14 -0400

    finished

--- script.js
+++ script.js
@@ -1,21 +1,73 @@
 var guidGenerator = (function () {
     'use strict'
-    const generateButton = document.getElementById('generate');
-    const resultTextbox = document.getElementById('result');
+    const generateElement = document.getElementById('generate');
+    const resultElement = document.getElementById('result');
+    const uppercaseElement = document.getElementById('uppercase');
+    const clipboardElement = document.getElementById('clipboard');
+    const copiedElement = document.getElementById('copied');
 
     function registerEvents() {
-        generateButton.addEventListener('click', () => {
-            resultTextbox.value = generateGuid();
+        generateElement.addEventListener('click', () => {
+            autoGenerateGuid();
+            hideCopiedNotification();
+        });
+
+        uppercaseElement.addEventListener('change', () => {
+            autoGenerateGuid();
+        });
+
+        clipboardElement.addEventListener('click', () => {
+            const textarea = document.createElement('textarea');
+            const guid = resultElement.value;
+
+            if (!guid) {
+                return;
+            }
+
+            textarea.value = guid;
+            document.body.appendChild(textarea);
+            textarea.select();
+
+            if (!navigator.clipboard) {
+                document.execCommand('copy');
+            } else {
+                navigator.clipboard.writeText(guid);
+            }
+
+            textarea.remove();
+            showCopiedNotification();
         });
     }
 
+    function hideCopiedNotification() {
+        if (copiedElement.classList.contains("show")) {
+            copiedElement.classList.remove("show");
+        }
+    }
+
+    function showCopiedNotification() {
+        hideCopiedNotification();
+        copiedElement.classList.add("show");
+        setTimeout(hideCopiedNotification, 1000);
+    }
+
     function generateGuid() {
-        return crypto.randomUUID();
+        let newGUID = crypto.randomUUID();
+        if (uppercaseElement.checked) {
+            newGUID = newGUID.toUpperCase();
+        }
+        crypto.randomUUID
+        return newGUID;
+    }
+
+    function autoGenerateGuid() {
+        resultElement.value = generateGuid();
     }
 
     return {
         init: function () {
             registerEvents();
+            autoGenerateGuid();
         }
     }
 })();
